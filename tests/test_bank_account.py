a976c2dc4d70dabbe8816249e84becce4008a9d2
ronseriajorge <ronseriajorge@gmail.com>
2024-09-08 13:12:56 -0500

    Se suben las pruebas de assert de unittest

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -17,18 +17,18 @@ class BankAccountTests(unittest.TestCase):
 
     def test_deposit(self):
         new_balance = self.account.deposit(500)
-        assert new_balance == 1500
+        self.assertEqual(new_balance, 1500, "El balance no es igual")
 
     def test_withdraw(self):
         new_balance = self.account.withdraw(200)
-        assert new_balance == 800
+        self.assertEqual(new_balance, 800, "El balance no es igual")
 
     def test_get_balance(self):
-        assert self.account.get_balance() == 1000
+        self.assertEqual(self.account.get_balance(), 1000)
 
     def test_transaction_log(self):
         self.account.deposit(500)
-        assert os.path.exists("transaction_log.txt")
+        self.assertTrue(os.path.exists("transaction_log.txt"))
 
     def test_count_transactions(self):
         assert self._count_lines(self.account.log_file) == 1
