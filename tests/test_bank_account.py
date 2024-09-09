3b748c49456b674e94f2c402c9739f007f84b9cb
ronseriajorge <ronseriajorge@gmail.com>
2024-09-08 19:16:30 -0500

    Se ajustan las pruebas de acuerdo al coverage de las pruebas

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -21,7 +21,9 @@ class BankAccountTests(unittest.TestCase):
         new_balance = self.account.deposit(500)
         self.assertEqual(new_balance, 1500, "El balance no es igual")
 
-    def test_withdraw_decreases_balance_by_withdraw_amount(self):
+    @patch("src.bank_account.datetime")
+    def test_withdraw_decreases_balance_by_withdraw_amount(self, mock_datetime):
+        mock_datetime.now.return_value.hour = 10
         new_balance = self.account.withdraw(200)
         self.assertEqual(new_balance, 800, "El balance no es igual")
 
@@ -37,7 +39,9 @@ class BankAccountTests(unittest.TestCase):
         self.account.deposit(500)
         self.assertEqual(self._count_lines(self.account.log_file), 2)
 
-    def test_withdraw_raises_error_when_insufficient_funds(self):
+    @patch("src.bank_account.datetime")
+    def test_withdraw_raises_error_when_insufficient_funds(self, mock_datetime):
+        mock_datetime.now.return_value.hour = 10
         with self.assertRaises(InsufficientFundsError):
             self.account.withdraw(2000)
 
@@ -56,19 +60,19 @@ class BankAccountTests(unittest.TestCase):
     @patch("src.bank_account.datetime")
     def test_withdraw_disallow_after_bussines_hours(self, mock_datetime):
         mock_datetime.now.return_value.hour = 18
-
         with self.assertRaises(WithdrawalTimeRestrictionError):
             self.account.withdraw(100)
 
-    def test_deposit_multiple_ammounts(self):
-        test_cases = [
-            {"ammount":100,  "expected":1100},
-            {"ammount":3000, "expected":4000},
-            {"ammount":4500 ,"expected":5500}
-        ]
 
+    def test_deposit_multiple_ammounts(self):
+
+        test_cases = [
+            {"ammount": 100, "expected": 1100},
+            {"ammount": 3000, "expected": 4000},
+            {"ammount": 4500, "expected": 5500},
+        ]
         for case in test_cases:
             with self.subTest(case=case):
-                self.account = BankAccount(balance=1000, log_file="transaction.txt")
+                self.account = BankAccount(balance=1000, log_file="transactions.txt")
                 new_balance = self.account.deposit(case["ammount"])
-                self.assertEqual(new_balance,case["expected"])
+                self.assertEqual(new_balance, case["expected"])
